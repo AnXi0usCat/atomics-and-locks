b143e84371e18dc1440edf9061584aa8cddf6d46
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-12-26 18:49:10 +0000

    rcu - with basic reference counting

---
+++ rcu/src/main.rs
@@ -0,0 +1,80 @@
+use std::{
+    ops::Deref,
+    sync::atomic::{AtomicPtr, AtomicU32, Ordering},
+};
+
+use atomic_wait::{wait, wake_one};
+
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Tested {
+    value: isize,
+}
+
+struct Rcu<T> {
+    pointer: AtomicPtr<T>,
+    readers: AtomicU32,
+}
+
+impl<T> Rcu<T> {
+    pub fn new(value: T) -> Self {
+        Rcu {
+            pointer: AtomicPtr::new(Box::into_raw(Box::new(value))),
+            readers: AtomicU32::new(0),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<'_, T> {
+        self.readers.fetch_add(1, Ordering::Release);
+        ReadGuard { rcu: self }
+    }
+
+    pub fn write(&self, value: T) {
+        let new_ptr = Box::into_raw(Box::new(value));
+        let mut r = self.readers.load(Ordering::Acquire);
+        loop {
+            if r > 0 {
+                wait(&self.readers, r);
+                r = self.readers.load(Ordering::Acquire);
+                continue;
+            }
+            let old_ptr = self.pointer.swap(new_ptr, Ordering::Release);
+            drop(unsafe { Box::from_raw(old_ptr) });
+            return;
+        }
+    }
+}
+
+impl<T> Drop for Rcu<T> {
+    fn drop(&mut self) {
+        drop(unsafe { Box::from_raw(self.pointer.load(Ordering::Acquire)) });
+    }
+}
+
+struct ReadGuard<'a, T> {
+    rcu: &'a Rcu<T>,
+}
+
+impl<T> Deref for ReadGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &*self
+                .rcu
+                .pointer
+                .load(Ordering::Acquire)
+                .as_ref()
+                .expect("Failed to obtain a a shared reference")
+        }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.rcu.readers.fetch_sub(1, Ordering::Release) == 1 {
+            wake_one(&self.rcu.readers);
+        }
+    }
+}
