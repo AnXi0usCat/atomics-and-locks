18271f620363a45423ccd58f1aa5b683a42faf5c
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-03-09 20:41:39 +0000

    simpiify code, prepare for hazard pointers

--- rcu/src/main.rs
+++ rcu/src/main.rs
@@ -1,11 +1,6 @@
 use std::{
-    ops::Deref,
-    sync::atomic::{AtomicPtr, AtomicU32, Ordering},
-    thread,
-    time::Duration,
+    marker::PhantomData, ops::Deref, sync::atomic::{AtomicPtr, Ordering}, thread, time::Duration
 };
-
-use atomic_wait::{wait, wake_one};
 
 fn main() {
     println!("Hello, world!");
@@ -13,9 +8,12 @@ fn main() {
 
     thread::scope(|s| {
         s.spawn(|| {
-            println!("{}", *rcu.read());
+            let mut reader = rcu.read();
+            println!("{}", *reader);
+            drop(reader);
             thread::sleep(Duration::from_secs(1));
-            println!("{}", *rcu.read());
+            reader = rcu.read();
+            println!("{}", *reader);
         });
         thread::sleep(Duration::from_millis(1));
         rcu.write(12);
@@ -23,85 +21,80 @@ fn main() {
 }
 
 struct Rcu<T> {
-    pointer: AtomicPtr<T>,
-    readers: AtomicU32,
+    ptr: AtomicPtr<T>
 }
-
-unsafe impl<T> Sync for Rcu<T> where T: Send + Sync {}
 
 impl<T> Rcu<T> {
     pub fn new(value: T) -> Self {
         Rcu {
-            pointer: AtomicPtr::new(Box::into_raw(Box::new(value))),
-            readers: AtomicU32::new(0),
+            ptr: AtomicPtr::new(Box::into_raw(Box::new(value)))
         }
     }
 
     pub fn read(&self) -> ReadGuard<'_, T> {
-        self.readers.fetch_add(1, Ordering::Release);
-        ReadGuard { rcu: self }
-    }
-
-    pub fn write(&self, value: T) {
-        let new_ptr = Box::into_raw(Box::new(value));
-        let mut r = self.readers.load(Ordering::Acquire);
-        let mut old_ptr = self.pointer.load(Ordering::Acquire);
         loop {
-            if r < 1 {
-                match self.pointer.compare_exchange(
-                    old_ptr,
-                    new_ptr,
-                    Ordering::Acquire,
-                    Ordering::Relaxed,
-                ) {
-                    Ok(_) => {
-                        drop(unsafe { Box::from_raw(old_ptr) });
-                        return;
-                    }
-                    Err(e) => {
-                        old_ptr = e;
-                        continue;
-                    }
-                }
+            let ptr = self.ptr.load(Ordering::Acquire);
+            if ptr.is_null() {
+                panic!("Failed to read pointer, poitner cannot be null");
             }
-            if r > 0 {
-                wait(&self.readers, r);
-                r = self.readers.load(Ordering::Acquire);
-                old_ptr = self.pointer.load(Ordering::Acquire);
-                continue;
+            // check again to make sure it didnt change
+            if self.ptr.load(Ordering::Acquire) == ptr {
+                return ReadGuard { 
+                    ptr, 
+                    _marker: PhantomData 
+                };
             }
         }
+    }
+
+    pub fn write(&self, value: T) -> *mut T {
+        let new_ptr = Box::into_raw(Box::new(value));
+        let old_ptr = self.ptr.swap(new_ptr, Ordering::AcqRel);
+        old_ptr
     }
 }
 
 impl<T> Drop for Rcu<T> {
     fn drop(&mut self) {
-        drop(unsafe { Box::from_raw(self.pointer.load(Ordering::Acquire)) });
+        drop(unsafe { Box::from_raw(self.ptr.load(Ordering::Acquire)) });
     }
 }
 
 struct ReadGuard<'a, T> {
-    rcu: &'a Rcu<T>,
+    ptr: *const T,
+    _marker: PhantomData<&'a T>
 }
 
 impl<T> Deref for ReadGuard<'_, T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
-        unsafe {
-            &*self
-                .rcu
-                .pointer
-                .load(Ordering::Acquire)
-                .as_ref()
-                .expect("Failed to obtain a a shared reference")
-        }
+        unsafe { &*self.ptr }
     }
 }
 
 impl<T> Drop for ReadGuard<'_, T> {
     fn drop(&mut self) {
-        if self.rcu.readers.fetch_sub(1, Ordering::Release) == 1 {
-            wake_one(&self.rcu.readers);
-        }
+        // will use this later to clear the hazard pointer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn simple_case() {
+        let rcu = Rcu::new(10);
+
+        thread::scope(|s| {
+            s.spawn(|| {
+                assert_eq!(10, *rcu.read());
+                thread::sleep(Duration::from_secs(1));
+                assert_eq!(12, *rcu.read());
+            });
+            thread::sleep(Duration::from_millis(1));
+            rcu.write(12);
+        });
     }
 }
